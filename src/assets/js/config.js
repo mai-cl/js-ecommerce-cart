c20ad104919e1b274c6ed272bc2f9189e3d12028
Rodrigo Canaviri <rodrigomt.cl@gmail.com>
2023-01-13 17:43:47 -0300

    update api

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,2 +1,1 @@
-/* export const API_URL = 'https://api-ecommerce-hw.herokuapp.com' */
-export const API_URL = 'http://localhost:3000'
+export const API_URL = 'https://api-products-olv8.onrender.com'
